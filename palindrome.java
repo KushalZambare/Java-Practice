d539fc788db2994c88c7aadaffa7b10ab80afa27
Kushal Zambare <90545038+KushalZambare@users.noreply.github.com>
2026-02-19 22:43:04 +0530

    Add files via upload

--- palindrome.java
+++ palindrome.java
@@ -1,27 +1,20 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class palindrome {
-
     public static void main(String[] args) {
-        System.out.println("Enter a string:");
-        Scanner sc=new Scanner(System.in);
-        String A=sc.next();
-        String res = "";
-        for (int i = 0; i < A.length(); i++) {
-        char c = A.charAt(i);
-        res = c + res;
-     }
-     
-     if (A.equals(res)){
-        System.out.println("Yes");
-     }
-     else{
-        System.out.println("No");
-     }
-     
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        str = str.replaceAll("[\\s+!@#$%^&*()_+-=?'',.><:;]", "").toLowerCase();
+        String reversedStr = new StringBuilder(str).reverse().toString();
+
+        if (str.equals(reversedStr)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+
+        sc.close();
+
     }
 }
-
-
-
