a39a4333b839187e12e9f6e1b5b8aec7d42cdbd0
Kushal Zambare <kushalzambare21@gmail.com>
2026-03-26 00:10:50 +0530

    leetcode_219

---
+++ containssuplicate2.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+public class containssuplicate2 {
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 1};
+        int k = 3;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (map.containsKey(nums[i])) {
+
+                int idx = map.get(nums[i]);
+
+                if (i - idx <= k) {
+                    System.out.println(true);
+                    return;
+                }
+            }
+
+            map.put(nums[i], i);
+        }
+
+        System.out.println(false);
+    }
+}
