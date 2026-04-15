028bf62f71a90d110b0f8e119525f7aed1646a41
Kushal Zambare <kushalzambare21@gmail.com>
2026-04-15 19:08:15 +0530

    Leetcode_49

--- groupanagram.java
+++ groupanagram.java
@@ -1,1 +1,24 @@
 import java.util.*;
+
+public class groupanagram{
+    public static void main(String[] args){
+        String[] str = {"eat","tea","tan","ate","nat","bat"};
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for(String word : str){
+            char[] chr = word.toCharArray();
+            Arrays.sort(chr);
+            String key = new String(chr);
+
+            if(!map.containsKey(key)){
+                map.put(key, new ArrayList<>());
+            }
+
+            map.get(key).add(word);
+
+        }
+        System.out.print(map.values());
+
+    }
+}
