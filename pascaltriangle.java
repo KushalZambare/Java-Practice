f5180ae14f8a6e2792b9be48d0e5bc3d94640d7e
Kushal Zambare <kushalzambare21@gmail.com>
2026-09-03 23:36:37 +0530

    Leetcode_118

---
+++ pascaltriangle.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+class pascaltriangle{
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(triangle.get(i - 1).get(j - 1) + triangle.get(i - 1).get(j));
+                }
+            }
+            triangle.add(row);
+        }
+
+        return triangle;
+    }
+    public static void main(String[] args) {
+        pascaltriangle pt = new pascaltriangle();
+        int numRows = 5; 
+        List<List<Integer>> result = pt.generate(numRows);
+        
+        for (List<Integer> row : result) {
+            System.out.println(row);
+        }
+    }
+}
