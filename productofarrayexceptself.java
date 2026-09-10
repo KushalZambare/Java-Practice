f7c09330d7e8042ba04a121bc1e9b929ac08b08f
Kushal Zambare <kushalzambare21@gmail.com>
2026-09-10 23:50:30 +0530

    leetcode 238

--- productofarrayexceptself.java
+++ productofarrayexceptself.java
@@ -3,19 +3,23 @@ import java.util.Arrays;
 class productofarrayexceptself {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int product = 1;
-        int res[] = new int[n];
-        Arrays.fill(res, 1);
-        for(int i = 0; i< n; i++){
-            if(nums[i] != 0){
-                
-            }
+        int[] res = new int[n];
+        int pdt = 1;
+        for (int i = 0; i < n; i++) {
+            res[i] = pdt;
+            pdt *= nums[i];
         }
+        pdt = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= pdt;
+            pdt *= nums[i];
+        }
+
         return res;
     }  
     public static void main(String[] args) {
             productofarrayexceptself obj = new productofarrayexceptself();
-            int[] nums = {1, 2, 0, 4};
+            int[] nums = {1, 2, 3, 4};
             int[] result = obj.productExceptSelf(nums);
             System.out.println(Arrays.toString(result));
     }       
